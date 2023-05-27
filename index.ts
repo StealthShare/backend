39e02cd6f84962803802b81afbc80c9279f2d542
damiansarnecki <xdamiansarnecki@gmail.com>
2023-05-27 03:07:40 +0200

    save

--- index.ts
+++ index.ts
@@ -6,6 +6,8 @@ const app: Express = express();
 import cors from "cors";
 import { CustomRequest, verifyToken } from "./middleware/auth";
 import { ERC1155_ABI } from "./abi/erc1155";
+import { MARKET_ADDRESS } from "./constants";
+import { MARKET_ABI } from "./abi/market";
 const port = process.env.PORT ?? 4000;
 
 const bodyParser = require("body-parser");
@@ -15,8 +17,9 @@ const generateNonce = (): string => {
   return nonce;
 };
 
-
-const provider = new ethers.JsonRpcProvider("https://matic-mumbai.chainstacklabs.com")
+const provider = new ethers.JsonRpcProvider(
+  "https://matic-mumbai.chainstacklabs.com"
+);
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -29,17 +32,19 @@ app.get("/:address/getNonce", (req: Request, res: Response) => {
   res.json({ seed: nonce });
 });
 
-app.get("/:token/download", verifyToken, async (req: Request, res: Response) => {
-  const token = req.params.token;
+app.get(
+  "/:token/download",
+  verifyToken,
+  async (req: Request, res: Response) => {
+    const token = req.params.token;
 
-  const contract = new ethers.Contract(token , ERC1155_ABI, provider);
+    const contract = new ethers.Contract(token, ERC1155_ABI, provider);
 
-  const balance = await contract.balanceOf((req as CustomRequest).address, 0);
+    const balance = await contract.balanceOf((req as CustomRequest).address, 0);
 
-  res.json({balance: Number(balance)});
-
-  
-});
+    res.json({ balance: Number(balance) });
+  }
+);
 
 app.post(
   "/login",
@@ -50,13 +55,32 @@ app.post(
     const { signedMessage, message, address } = req.body;
     const recoveredPublicKey = ethers.verifyMessage(message, signedMessage);
 
-	if (recoveredPublicKey.toLowerCase() != address.toLowerCase()) {
+    if (recoveredPublicKey.toLowerCase() != address.toLowerCase()) {
       res.status(401).json({ error: "Invalid signer" });
     }
 
     const token = jwt.sign({ address }, "1234", { expiresIn: "1h" });
 
     res.json(token);
+  }
+);
+
+const packageContract = new ethers.Contract(
+  MARKET_ADDRESS,
+  MARKET_ABI,
+  provider
+);
+
+packageContract.on(
+  "NewListing(address, address, string, uint256)",
+  (user: string, token: string, uri : number, price: number) => {
+    console.log("New token listed.", "Event data:", {
+      user,
+      token,
+      price,
+      uri,
+	  supply
+    });
   }
 );
 
